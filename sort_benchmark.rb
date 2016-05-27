308a656e47eff1d6f4fbcb7237c6100cc6937615
Sriram Thaiyar <sthaiyar@on-site.com>
2016-05-27 00:08:28 -0700

    Initial

---
+++ sort_benchmark.rb
@@ -0,0 +1,33 @@
+#! /usr/bin/env ruby
+
+require 'benchmark'
+
+class Test
+  attr_reader :a
+
+  def initialize(a)
+    @a = a
+  end
+
+  def <=>(other)
+    a <=> other.a
+  end
+end
+
+MAX = 5_000_000
+
+sorted_ints   = (1..MAX).to_a
+random_ints   = (1..MAX).to_a.shuffle
+random_floats = (1..MAX).to_a.map { |x| x.to_f }.shuffle
+random_objs   = (1..MAX).to_a.map { |x| Test.new(x) }.shuffle
+
+puts "Iterations: #{MAX}"
+puts defined?(JRUBY_VERSION) ? "JRUBY_VERSION: #{JRUBY_VERSION}" : "MRI Ruby"
+puts "RUBY_VERSION: #{RUBY_VERSION}"
+
+Benchmark.bmbm do |x|
+  x.report("sorted_ints") { sorted_ints.sort }
+  x.report("random_ints") { random_ints.sort }
+  x.report("random_floats") { random_floats.sort }
+  x.report("random_objs") { random_objs.sort }
+end
