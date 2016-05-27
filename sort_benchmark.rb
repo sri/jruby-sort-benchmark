031e6180faaf20c585e63c298b114cdf778c6886
Sriram Thaiyar <sthaiyar@on-site.com>
2016-05-27 11:21:13 -0700

    Add small_arrays

--- sort_benchmark.rb
+++ sort_benchmark.rb
@@ -16,11 +16,11 @@ end
 
 MAX = 5_000_000
 
+small_arrays  = (1..(MAX / 10)).to_a.map { |x| (1..rand(16)).to_a.shuffle  }
 sorted_ints   = (1..MAX).to_a
 random_ints   = (1..MAX).to_a.shuffle
 random_floats = (1..MAX).to_a.map { |x| x.to_f }.shuffle
 random_objs   = (1..MAX).to_a.map { |x| Test.new(x) }.shuffle
-
 
 puts "java.runtime.version: #{ENV_JAVA['java.runtime.version']}"
 puts "java.vm.version: #{ENV_JAVA['java.vm.version']}"
@@ -33,4 +33,5 @@ Benchmark.bmbm do |x|
   x.report("random_ints") { random_ints.sort }
   x.report("random_floats") { random_floats.sort }
   x.report("random_objs") { random_objs.sort }
+  x.report("small_arrays") { small_arrays.each { |x| x.sort } }
 end
